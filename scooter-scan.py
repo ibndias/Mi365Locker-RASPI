0120e50f50c4d4127d3877af8d1426eec0df8c64
Louis Linux <me@louiscb.com>
2019-03-27 16:58:37 +0100

    Fixed up code and adder timeout

--- scooter-scan.py
+++ scooter-scan.py
@@ -2,44 +2,60 @@ from bluepy import btle
 from bluepy.btle import Scanner, DefaultDelegate
 from bluepy.btle import BTLEDisconnectError
 from bluepy.btle import BTLEGattError
-
 import codecs
+import signal
+import sys
+import os
 
-counter = 0
+TIMEOUT_LENGTH = 1
+LOCK = "55aa032003700168ff"
+UNLOCK = "55aa032003710167ff"
+
+print(sys.argv[1:])
+
+def timeout_handler(signum, frame):
+    raise TimeoutError
+
+
+signal.signal(signal.SIGALRM, timeout_handler)
 
 
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
 
-   # def handleDiscovery(self, dev, isNewDev, isNewData):
 
-
-def lock_device(dev):
-    peri = btle.Peripheral(dev)
+def write_command(device, command):
+    signal.alarm(TIMEOUT_LENGTH)
+    peri = btle.Peripheral(device)
     characteristics = peri.getCharacteristics(uuid="6e400002-b5a3-f393-e0a9-e50e24dcca9e")[0]
-    characteristics.write(codecs.decode('55aa032003700168ff', 'hex'))
+    characteristics.write(codecs.decode(command, 'hex'))
     peri.disconnect()
-    print("Succesfully locked device!")
-
-
-def unlock_device(dev):
-    print("Unlocking device")
-    peri = btle.Peripheral(dev)
-    characteristics = peri.getCharacteristics(uuid="6e400002-b5a3-f393-e0a9-e50e24dcca9e")[0]
-    characteristics.write(codecs.decode('55aa032003710167ff', 'hex'))
-    peri.disconnect()
-  #  Unlock = "55aa032003710167ff".decode("hex")
+    print("Success!")
+    f = open("scootersAddr.txt", "a")
+    f.write(device.addr + '\n')
+    f.close()
 
 
 scanner = Scanner().withDelegate(ScanDelegate())
 devices = scanner.scan(2)
 
-print("Found ", devices.__sizeof__(), " device/s in bluetooth area")
+print("Found ", len(devices), " device/s in bluetooth area")
+
+fileExists = os.path.exists('./scootersAddr.txt')
+
+if fileExists:
+    with open('scootersAddr.txt') as file:
+        knownAddr = file.readlines()
+
+    for device in devices:
+        for addr in knownAddr:
+            if device.addr == addr:
+                devices = device + devices
 
 for device in devices:
     try:
         print("Attempting to lock device ", device.addr, device.getScanData())
-        lock_device(device)
-    except (BTLEDisconnectError, BTLEGattError):
+        write_command(device, UNLOCK)
+    except (BTLEDisconnectError, BTLEGattError, TimeoutError):
         print("Couldn't connect")
