35603b9030ee5c63ceb5b366d70e66db32e3619b
Louis Linux <me@louiscb.com>
2019-03-28 12:26:00 +0100

    Added command line argument functionality for scan choice and specific command

--- scooter-scan.py
+++ scooter-scan.py
@@ -7,13 +7,27 @@ import signal
 import sys
 import os
 
-TIMEOUT_LENGTH = 1
+# CHARACTERISTIC
+WRITE_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
+
+# COMMANDS
 LOCK = "55aa032003700168ff"
 UNLOCK = "55aa032003710167ff"
 
-print(sys.argv[1:])
+# CONSTANTS
+TIMEOUT_LENGTH = 3
+FILE_NAME = "scootersAddr.txt"
+COMMAND = ""
 
-def timeout_handler(signum, frame):
+if sys.argv[2] == "lock":
+    COMMAND = LOCK
+elif sys.argv[2] == "unlock":
+    COMMAND = UNLOCK
+else:
+    raise Exception('Command not recognised')
+
+
+def timeout_handler(signum, timeout_handler):
     raise TimeoutError
 
 
@@ -25,37 +39,73 @@ class ScanDelegate(DefaultDelegate):
         DefaultDelegate.__init__(self)
 
 
-def write_command(device, command):
-    signal.alarm(TIMEOUT_LENGTH)
-    peri = btle.Peripheral(device)
-    characteristics = peri.getCharacteristics(uuid="6e400002-b5a3-f393-e0a9-e50e24dcca9e")[0]
-    characteristics.write(codecs.decode(command, 'hex'))
-    peri.disconnect()
-    print("Success!")
-    f = open("scootersAddr.txt", "a")
-    f.write(device.addr + '\n')
-    f.close()
-
-
 scanner = Scanner().withDelegate(ScanDelegate())
 devices = scanner.scan(2)
 
-print("Found ", len(devices), " device/s in bluetooth area")
 
-fileExists = os.path.exists('./scootersAddr.txt')
+def add_addr_to_known(dev_addr):
+    file_exists = os.path.exists('./' + FILE_NAME)
 
-if fileExists:
-    with open('scootersAddr.txt') as file:
-        knownAddr = file.readlines()
+    if file_exists:
+        with open(FILE_NAME) as file:
+            addresses = file.readlines()
 
-    for device in devices:
-        for addr in knownAddr:
-            if device.addr == addr:
-                devices = device + devices
+        for addr in addresses:
+            if addr == dev_addr:
+                return None
 
-for device in devices:
-    try:
-        print("Attempting to lock device ", device.addr, device.getScanData())
-        write_command(device, UNLOCK)
-    except (BTLEDisconnectError, BTLEGattError, TimeoutError):
-        print("Couldn't connect")
+    f = open(FILE_NAME, "a")
+    f.write(dev_addr + '\n')
+    f.close()
+
+
+def write_command(dev, command):
+    signal.alarm(TIMEOUT_LENGTH)
+    peri = btle.Peripheral(dev)
+    characteristics = peri.getCharacteristics(uuid=WRITE_UUID)[0]
+    characteristics.write(codecs.decode(command, 'hex'))
+    peri.disconnect()
+    print("Success!")
+    add_addr_to_known(dev.addr)
+
+
+def write_devices(devs, command):
+    for dev in devs:
+        try:
+            print("Attempting to send command to device ", dev.addr, dev.getScanData())
+            write_command(dev, command)
+        except (BTLEDisconnectError, BTLEGattError, TimeoutError):
+            print("Couldn't connect")
+
+
+def get_known_addr(devs):
+    file_exists = os.path.exists('./' + FILE_NAME)
+    known_devices = []
+
+    if file_exists:
+        with open(FILE_NAME) as file:
+            known_addr = file.readlines()
+
+        for device in devs:
+            for addr in known_addr:
+                if device.addr == addr:
+                    known_devices.append(device)
+    else:
+        raise Exception('No saved addresses in file ' + FILE_NAME)
+
+    return known_devices
+
+
+if sys.argv[1] == "scan":
+    print("Scanning ", len(devices), " device/s in bluetooth area")
+    write_devices(devices, COMMAND)
+elif sys.argv[1] == "saved":
+    knownDevices = get_known_addr(devices)
+
+    if knownDevices:
+        print("Scanning ", len(knownDevices), " device/s in bluetooth area")
+        write_devices(knownDevices, COMMAND)
+    else:
+        raise Exception('Could not find any known devices in area')
+else:
+    raise Exception('incorrect arguments')
